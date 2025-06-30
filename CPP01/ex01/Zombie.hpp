55f8e653b2c67f0a5ee8bf2702a6adfc173aa150
larocqueg <portugalgabriel2021@gmail.com>
2025-06-30 15:43:23 +0100

    starting ex01

---
+++ CPP01/ex01/Zombie.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-la-r <gde-la-r@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 15:33:40 by gde-la-r          #+#    #+#             */
+/*   Updated: 2025/06/30 15:34:52 by gde-la-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+  private:
+    std::string name;
+  public:
+    Zombie();
+    ~Zombie();
+    Zombie(std::string name);
+    void  announce(std::string name);
+};
+
+Zombie  *zombieHorde(int N, std::string name);
+
+#endif
