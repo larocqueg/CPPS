e4d00a7a66ea7876ef06609b1642d8326115a386
larocqueg <portugalgabriel2021@gmail.com>
2025-07-21 23:48:20 +0100

    ex00 cpp02 done

--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -10,3 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <string>
+#include <iostream>
+
+class Fixed {
+  private:
+    int               val;
+    static const int  bits = 8;
+  public:
+    Fixed();
+    Fixed(const Fixed& other);
+    ~Fixed();
+    Fixed& operator = (const Fixed& other);
+    int   getRawBits(void) const;
+    void  setRawBits(int const raw);
+};
+
+#endif
--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -14,5 +14,13 @@
 
 int main(void)
 {
+  Fixed a;
+  Fixed b(a);
+  Fixed c = b;
 
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+  
+  return (0);
 }
