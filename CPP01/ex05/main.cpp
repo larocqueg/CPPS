0a51e2d15ab2f01b1d19e8627559a003fabfaf1b
larocqueg <portugalgabriel2021@gmail.com>
2025-07-21 14:10:14 +0100

    cpp01 ex05 done

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -10,3 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int main(int ac, char **av)
+{
+  Harl harl;
+  if (ac == 2) {
+    harl.complain(av[1]);
+    return (0);
+  }
+  else {
+    std::cout << "Error: Usage: ./Harl 'command'"
+      << "\n for commands you can use:\n"
+      << "DEBUG\nINFO\nWARNING\nERROR" << std::endl;
+    return (1);
+  }
+}
