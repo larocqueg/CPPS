9ec52f502ed6b242d2055cdd035104858f8d2381
larocqueg <portugalgabriel2021@gmail.com>
2025-07-21 11:59:25 +0100

    cpp01 ex04 done

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-la-r <gde-la-r@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 10:15:38 by gde-la-r          #+#    #+#             */
+/*   Updated: 2025/07/21 10:22:04 by gde-la-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main(int ac, char **av)
+{
+  std::string file1_name;
+  std::string file2_name;
+  std::string text;
+  std::string s1;
+  std::string s2;
+  std::ifstream infile;
+  std::ofstream outfile;
+  
+  if (ac == 4) {
+    file1_name = av[1];
+    file2_name = file1_name + ".replace";
+    s1 = av[2];
+    s2 = av[3];
+
+    infile.open(file1_name.c_str(), std::ios::in);
+    outfile.open(file2_name.c_str(), std::ios::out);
+    if (!infile.is_open()) {
+      std::cout << "Error while opening file " << file1_name << std::endl;
+      return (1);
+    }
+    while (std::getline(infile, text)) {
+      size_t pos = 0;
+      while ((pos = text.find(s1, pos)) != std::string::npos) {
+        text.erase(pos, s1.length());
+        text.insert(pos, s2);
+        pos += s2.length();
+      }
+      outfile << text << std::endl;
+    }
+    infile.close();
+    outfile.close();
+    return (0);
+  }
+  std::cout << "Error: Usage: ./replace 'filename' 'string1' 'string2'" << std::endl;
+  return (1);
+}
