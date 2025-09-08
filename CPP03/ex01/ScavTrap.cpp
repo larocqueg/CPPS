24f25c92c6a6253aa4619760505f893d1050581d
larocqueg <portugalgabriel2021@gmail.com>
2025-09-08 18:20:14 +0100

    for the standard way, ScavTrap is missing copy constructor

--- CPP03/ex01/ScavTrap.cpp
+++ CPP03/ex01/ScavTrap.cpp
@@ -5,57 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gde-la-r <gde-la-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/08 16:04:00 by gde-la-r          #+#    #+#             */
-/*   Updated: 2025/09/08 16:15:54 by gde-la-r         ###   ########.fr       */
+/*   Created: 2025/09/08 18:07:07 by gde-la-r          #+#    #+#             */
+/*   Updated: 2025/09/08 18:19:45 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-ScavTrap::ScavTrap() : ClapTrap("Unknown", 10, 50, 20)
+ScavTrap::ScavTrap(void) : ClapTrap("Unknown")
 {
-  std::cout << GREEN << "ScavTrap " << this->_name << " was created!"
+  std::cout << YELLOW << "ScavTrap default constructor called!"
     << RESET << std::endl;
+  
+  this->_atk = 20;
+  this->_ep = 50;
+  this->_hp = 100;
+
+  std::cout << GREEN << "ScavTrap " << this->_name << " was created!"
+    << RESET << std::end;;
 }
 
-ScavTrap::ScavTrap(std::string name) : ClapTrap(name, 100, 50, 20)
+ScavTrap::ScavTrap(std::string const& name) : ClapTrap(name)
 {
-  std::cout << GREEN << "ScavTrap " << this->_name << " was created!"
+  std::cout << YELLOW << "ScavTrap parametized constructor called!"
     << RESET << std::endl;
+  
+  this->_atk = 20;
+  this->_ep = 50;
+  this->_hp = 100;
+
+  std::cout << GREEN << "ScavTrap " << this->_name << " was created!"
+  << RESET << std::endl;
 }
 
-ScavTrap::ScavTrap(const ScavTrap& original)
+ScavTrap& Scavtrap :: operator = (const Scavtrap& original)
 {
-  *this = original;
-}
-ScavTrap& ScavTrap :: operator = (const ScavTrap& original)
-{
-  if (*this != original)
-  {
-    ClapTrap::_name = original._name;
-    this->_atk = original.atk;
-    this->_hp = original._hp;
-    this->_ep = original._ep;
-  }
+  std::cout << YELLOW << "Scavtrap copy assigment operator called!"
+    << RESET << std::endl;
+
+  if (this != original)
+    ClapTrap::operator=(original);
   return (*this);
+
 }
 
 ScavTrap::~ScavTrap()
 {
+  std::cout << YELLOW << "Scavtrap destructor called!" << RESET << std::endl;
+  
   std::cout << RED << "ScavTrap " << this->_name << " was destroyed!"
-    << RESET << std::endl;
-}
-
-void  ScavTrap::attack(std::string target)
-{
-  if (this->_ep <= 0 || this->_hp <= 0)
-    std::cout << RED << "ScavTrap " << this->_name
-      << " Couldn't attack, not enough energy or hit points!"
-      << RESET << std::endl;
-  else
-  {
-    std::cout << "ScavTrap " << this->_name << " attacked "
-    << target << " causing " << this->_atk << "points of damage!"
-    << std::endl;
-  }
+  << RESET << std::endl;
 }
